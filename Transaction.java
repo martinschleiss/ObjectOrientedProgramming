45f3db2483a5b22bf57acc1b2ee03c4f7bf625f7
Martin Schleiss <e1126579@student.tuwien.ac.at>
2012-10-30 23:11:31 +0100

    Kommentare bezueglich Vor- und Nachbedingungen bei Transaction, Income und Expense hinzugefuegt

--- Transaction.java
+++ Transaction.java
@@ -10,10 +10,21 @@ abstract public class Transaction {
 	protected Event ev;
 	protected Date date;
 	
+	/**
+	 * Vorbedingung: value muss ein positiver Wert sein
+	 * Nachbedingung: die Klasse muss erfolgreich mit dem Wert value in this.value initialisiert sein
+	 * @param value
+	 */	
 	public Transaction(double value) {
 		this.value = value;
 	}
 	
+	/**
+	 * Vorbedingung: value muss ein positiver Wert sein, date ist das Datum an dem die Transaktion stattgefunden hat
+	 * Nachbedingung: die Klasse muss erfolgreich mit den Werten value und date in this.value und this.date initialisiert sein
+	 * @param value
+	 * @param date
+	 */
 	public Transaction(double value, Date date) {
 		this(value);
 		this.date = date;
@@ -21,9 +32,19 @@ abstract public class Transaction {
 	
 	abstract public double getValue();
 	
+	/**
+	 * Nachbedingung: Liefert das Event mit dem die Transaktion assoziiert ist.
+	 * @return
+	 */
 	public Event getCorrespondingEvent() {
 		return this.ev;
 	}
+	
+	/**
+	 * Vorbedinung: ev muss vom Typ Event sein. 
+	 * Nachbedingung: das Event mit dem die Transaktion assoziiert werden soll, muss in this.ev gespeichert sein
+	 * @param ev
+	 */
 	public void setCorrespondingEvent(Event ev) {
 		this.ev = ev;
 	}
