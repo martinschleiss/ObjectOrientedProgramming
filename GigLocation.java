c26f602d71804963282cef80d85e4ef7c4430145
Rafael Konlechner <r.konlechner@gmail.com>
2012-11-07 02:30:58 +0100

    finale Aenderungen

--- GigLocation.java
+++ GigLocation.java
@@ -8,17 +8,42 @@ public class GigLocation extends Location{
 	
 	private int seatings;
 	
+	/**
+	 * Vorbedingungen: 
+	 * Vorbedinugungen des Superkonstruktors und	
+	 * @param seatings 	> 0
+	 * 
+	 * Nachbedingung:
+	 * Objekt wurde korrekt initialisiert.
+	 */
 	public GigLocation(int district, int size, int stageSize, int seatings){
 		super(district, size, stageSize);
 		this.setSeatings(seatings);
 	}
+	
+	/**
+	 * Nachbedingung:
+	 * @return Anzahl der Sitzplaetze
+	 */
 	public int getSeatings() {
 		return seatings;
 	}
-	//seatings>0
+
+	/**
+	 * Vorbedingung:
+	 * @param seatings		> 0
+	 * 
+	 * Nachbedingung:
+	 * seatings wurden gespeichert.
+	 */
 	public void setSeatings(int seatings) {
 		this.seatings = seatings;
 	}
+	
+	/**
+	 * Nachbedingung:
+	 * @return String-Repraesentation des Objekts
+	 */
 	public String toString() {
 		
 		return super.toString() + ", Seatings: " + seatings;
--- RehearsalLocation.java
+++ RehearsalLocation.java
@@ -10,9 +10,12 @@
 		
 		/**
 		 * Vorbedingungen: 
-		 * 			Vorbedinugungen des Superkonstruktors und	
-		 * 				@param distanceToCenter > 0
-		 * 				@param wallColor muss einer Farbe entsprechen
+		 * Vorbedinugungen des Superkonstruktors und	
+		 * @param distanceToCenter 	> 0, in Meter
+		 * @param wallColor 		muss einer Farbe entsprechen
+		 * 
+		 * Nachbedingung:
+		 * Objekt wurde korrekt initialisiert.
 		 */
 		public RehearsalLocation(int district, int size, int stageSize, int distanceToCenter,String wallColor){
 			super(district, size, stageSize);
@@ -20,19 +23,28 @@
 			this.setWallColor(wallColor);
 		}
 		
+		/**
+		 * Nachbedingung:
+		 * @return Abstand zum Zentrum in Meter
+		 */
 		public int getDistanceToCenter() {
 			return distanceToCenter;
 		}
 		
 		/**
 		 * Vorbedingungen:
-		 * @param distanceToCenter > 0
+		 * @param distanceToCenter > 0, in Meter
 		 * 
 		 * Nachbedingungen: distanceToCenter wird als neue Distanz gespeichert
 		 */
 		public void setDistanceToCenter(int distanceToCenter) {
 			this.distanceToCenter = distanceToCenter;
 		}
+		
+		/**
+		 * Nachbedingung:
+		 * @return Wandfarbe
+		 */
 
 		public String getWallColor() {
 			return wallColor;
@@ -40,14 +52,18 @@
 
 		/**
 		 * Vorbedingungen:
-		 * @param wallColor entspricht einer Farbe
+		 * @param wallColor 	entspricht einer Farbe
 		 * 
-		 * Nachbedingungen: wallColor wird als neue Farbe gespeichert
+		 * Nachbedingungen: wallColor wird gespeichert
 		 */
 		public void setWallColor(String wallColor) {
 			this.wallColor = wallColor;
 		}
 		
+		/**
+		 * Nachbedingung:
+		 * @return String-Repraesentation des Objekts
+		 */
 		public String toString() {
 			
 			return super.toString() + ", Distance To Center: " + distanceToCenter +" Wall Color: "+wallColor;
