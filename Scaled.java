09f4bb01e365c3f5dc7e365d420f036bf27010de
Martin Schleiss <schleissmartin@gmail.com>
2012-11-14 01:00:10 +0100

    temporaere Scaled Implementation

--- Scaled.java
+++ Scaled.java
@@ -1,9 +1,67 @@
-public class Scaled<P> {
+import java.util.ArrayList;
+import java.util.Scanner;;
+
+public class Scaled<T extends Pict> implements Pict {
 	
-	P teil;
+	private int height;
+	private int width;
+	private ArrayList<? extends Pict> list;
+	private int boxWidth;
+	private int boxHeight;
 	
-	public Scaled (P teil) {
+	public Scaled(ArrayList<? extends Pict> list, int height, int width) {
+		this.height = height;
+		this.width = width;
+		this.list = list;
+		this.boxHeight = getMaximumBoxHeight(list);
+		this.boxWidth = getMaximumBoxWidth(list);
+	}
+	
+	private int getMaximumBoxHeight(ArrayList<? extends Pict> l) {
+		int maximum = 0;
 		
-		this.teil = teil;
+		for(Pict elem : l) {
+			int temp = 0;
+			String s = elem.toString();
+			Scanner scan = new Scanner(s);
+			while(scan.hasNext()) {
+				scan.nextLine();
+				temp++;
+			}
+			
+			if(temp > maximum) {
+				maximum = temp;
+			}
+		}
+		
+		return maximum;
+	}
+	
+	private int getMaximumBoxWidth(ArrayList<? extends Pict> l) {
+		return 0;
+	}
+	
+	public void scale(double factor) {
+		
+		for(Pict elem : list) {
+			elem.scale(factor);
+		}
+	
+	}
+	
+	public String toString() {
+		String s = "";
+		
+		for(int h = 0; h < height; h++) {
+			
+			for(int w = 0; w < width; w++) {
+				
+				//for(int bh = 0; )
+				
+			}
+			
+		}
+		
+		return s;
 	}
 }
