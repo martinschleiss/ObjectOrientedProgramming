a0a65532dc078a0c2676926beb0d31eacba57ca0
Martin Schleiss <e1126579@student.tuwien.ac.at>
2012-11-30 16:56:09 +0100

    weitere Unvollstaendigkeiten hinzugefuegt

--- Auto.java
+++ Auto.java
@@ -4,21 +4,24 @@ public class Auto implements Runnable{
 	private Feld f;
 	private final Fahrbahn fahrbahn;
 	private enum ausrichtung {NORD, OST, SUED, WEST};
+	private ausrichtung akutell;
+	private int punkte = 0;
 	
 	public Auto(Fahrbahn fahrbahn, Feld startFeld) {
 		this.fahrbahn = fahrbahn;
 		this.f = startFeld;
+		this.akutell = ausrichtung.NORD;
 	}
 	
 	public void run() {
 		
 	}
 	
-	public void wechsleZuFeld(Feld ziel) {
-		
+	public final void wechsleZuFeld(Feld ziel) {
+		f = ziel;
 	}
 	
-	public Feld aktuellesFeld() {
+	public final Feld aktuellesFeld() {
 		return f;
 	}
 	
@@ -26,11 +29,11 @@ public class Auto implements Runnable{
 		
 	}
 	
-	public void plusPunkt() {
-	
+	public final void plusPunkt() {
+		++punkte;
 	}
 	
-	public void minusPunkt() {
-		
+	private final void minusPunkt() {
+		--punkte;
 	}
 }
