4314a6aed8112f060c07b0789cb3954b95f05f76
Rafael Konlechner <r.konlechner@gmail.com>
2012-12-12 00:17:54 +0100

    tmp

--- TraktorErweiterungDrillmaschine.java
+++ TraktorErweiterungDrillmaschine.java
@@ -1,17 +1,33 @@
+/**
+ * Drillmaschine als Erweiterung fuer Traktoren
+ */
 
 public class TraktorErweiterungDrillmaschine extends TraktorErweiterung {
 
-	private int saeschare;
+	private int saeschare; // > 0
+	
 	public TraktorErweiterungDrillmaschine(int saeschare) {
+	
 		this.saeschare=saeschare;
 	}
 	
+	/**
+	 * Liefert Groesse des Ausmasses
+	 * @return ausmass >= 0
+	 * 			nur ganzzahlige Genauigkeit
+	 */
 	@Override
 	public double erweiterungsAusmass() {
+		
 		return saeschare;
 	}
+	
+	/**
+	 * Liefert String-Repraesentation der Klasse
+	 */
 	@Override
 	public String toString(){
+		
 		return "Drillmaschine";
 	}
 }
--- TraktorErweiterungDuengestreuer.java
+++ TraktorErweiterungDuengestreuer.java
@@ -7,11 +7,22 @@ public class TraktorErweiterungDuengestreuer extends TraktorErweiterung {
 		this.fassungskapazitaet=fassungskapazitaet;
 	}
 	
+	/**
+	 * Liefert Groesse des Ausmasses
+	 * @return ausmass >= 0
+	 * 			nur ganzzahlige Genauigkeit
+	 */
 	public double erweiterungsAusmass() {
+		
 		return fassungskapazitaet;
 	}
+	
+	/**
+	 * Liefert String-Repraesentation der Klasse
+	 */
 	@Override
 	public String toString(){
+		
 		return "Duengestreuer";
 	}
 }
