d7d181f1619476aea9a5320e36d1ad743d0bd85b
Rafael Konlechner <r.konlechner@gmail.com>
2012-12-12 15:38:39 +0100

    Keksdose

---
+++ Keksdose.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+public class Keksdose {
+
+	private ArrayList<Keks> kekse;
+
+	public Keksdose() {
+
+		kekse = new ArrayList<Keks>();
+	}
+
+	public void befuelle(Collection<Keks> c) {
+
+		kekse.addAll(c);
+	}
+
+	public void inhalt() {
+
+		for (Keks k : kekse) {
+
+			System.out.println(k);
+		}
+	}
+}
